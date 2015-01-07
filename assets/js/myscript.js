169df5ba5f4998c3aa105fce09c60b7db52588eb
Florian Perrot <contact@florian-perrot.fr>
2015-01-07 09:52:38 +0100

    Ajout d'un evenement jquery - ajout une feuille sur le footer au clic sur le 1 elem du menu

---
+++ assets/js/myscript.js
@@ -0,0 +1,21 @@
+function stringGen(len){
+  var text = " ";
+  var charset = "abcdefghijklmnopqrstuvwxyz";
+  for( var i=0; i < len; i++ )
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+  return text;
+}
+
+function ajoutFeuille(monSol){
+  monSol.append('<span class="feuille">'+stringGen(5)+'</span>');
+}
+
+$(document).ready(function(){
+  var $feuilles = $('.feuilles');
+  var $menu1 = $('.menu1');
+
+  $menu1.click(function(){
+    console.log('Ajout d\'une feuille.');
+    ajoutFeuille($feuilles);
+  });
+});
