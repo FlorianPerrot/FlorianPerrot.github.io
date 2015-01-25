6b9738d7fe88ba479b2b1210e7f9b0ad6ebc6ccb
Florian Perrot <contact@florian-perrot.fr>
2015-01-25 22:36:21 +0100

    Remise en forme du bouton menu en mode tablette. Ajout des boutons pour les interactions avec le footer (ajout et supr d'arbre)

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -6,25 +6,47 @@ function stringGen(len){
   return text;
 }
 
-function ajoutFeuille(monSol){
-  monSol.append('<span class="feuille">'+stringGen(5)+'</span>');
+function ajoutArbre(monSol){
+  monSol.append('<span>'+stringGen(5)+'</span>');
+}
+
+function suprArbre(monSol){
+  monSol.children("span").last().remove();
 }
 
 $(document).ready(function(){
-  var $feuilles = $('.feuilles');
+  var $arbres = $('footer .arbres');
   var $menu1 = $('.menu1');
   var $video = $("#bgvid");
+  var $seed = $('footer .seed');
+  var $axe = $('footer .axe');
 
-  $feuilles.click(function(){
-    console.log('Ajout d\'une feuille.');
-    ajoutFeuille($feuilles);
+  /* GESTION DES ARBRES */
+  $seed.click(function(){
+    if($arbres.width() > (($arbres.children().length+1)*$arbres.children().last().width()) ){
+      ajoutArbre($arbres);
+      console.log("Ajout d\'un arbre");
+    }
+  });
+  $axe.click(function(){
+    console.log('supr un arbre.');
+    suprArbre($arbres);
   });
 
+  /* GESTION DU MENU */
   $('#small-menu').click(function() {
     console.log("Action sur le menu");
     toggleNav();
   });
+  $('.menu3').click(function(){
+    $('#site-wrapper').removeClass('show-nav');
+  });
+  $('.menu1').click(function(){
+    $video.get(0).play();
+    $('#site-wrapper').removeClass('show-nav');
+  });
 
+  /* GESTION VIDEO */
   $video.click(function(){
     if($video.get(0).paused){
       console.log("Pause video");
@@ -34,8 +56,4 @@ $(document).ready(function(){
       $video.get(0).pause();}
   });
 
-  $('.menu1').click(function(){
-    $video.get(0).play();
-    $('#site-wrapper').removeClass('show-nav');
-  })
 });
